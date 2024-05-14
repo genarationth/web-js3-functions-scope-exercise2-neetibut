72e3342be90bfe2b604214b8632baada6fd873a1
neetibut <neetibut@gmail.com>
2024-05-14 23:23:36 +0700

    completed

---
+++ script.js
@@ -0,0 +1,17 @@
+// function isNumEven(num) {
+//   if (num % 2 === 0) {
+//     return `${num} is an even number`;
+//   } else {
+//     return `${num} is an odd number`;
+//   }
+// }
+
+const isNumEven = (num) => {
+  if (num % 2 === 0) {
+    return `${num} is an even number`;
+  } else {
+    return `${num} is an odd number`;
+  }
+};
+
+console.log(isNumEven(1234567890));
